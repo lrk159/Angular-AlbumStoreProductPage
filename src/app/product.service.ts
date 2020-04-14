61429a7270d398e4f543e1e5b72cc8eeeb3df28b
Luc Kuckartz <luc.kuckartz@sasktel.net>
2020-04-14 16:00:51 -0600

    fix import issue.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 
 
@@ -12,7 +12,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(map(response => response.json()));
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
